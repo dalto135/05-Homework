2ede96dc4679818155c32b43f7819beb21709019
Dalton Wilkins <wilkins.271@osu.edu>
2021-02-25 20:57:33 -0500

    fiddling with the for loop

--- Develop/script.js
+++ Develop/script.js
@@ -4,58 +4,78 @@ console.log(mom);
 let h = moment().format("h");
 console.log(h);
 
-let H = moment().format("H");
+// let H = moment().format("H");
 
 let a = moment().format("A");
 console.log(a);
 
-let hour = document.querySelector(".hour");
-console.log(hour.parentNode);
+// let hour = document.querySelector(".hour");
+// console.log(hour.parentNode);
 
+let twelveam = document.querySelector("#twelveam");
+let oneam = document.querySelector("#oneam");
+let twoam = document.querySelector("#twoam");
+
+let nine = document.querySelector("#nine");
+let ten = document.querySelector("#ten");
+let eleven = document.querySelector("#eleven");
 let twelve = document.querySelector("#twelve");
 let one = document.querySelector("#one");
 let two = document.querySelector("#two");
-let pm = document.querySelector("#pm");
+let three = document.querySelector("#three");
+let four = document.querySelector("#four");
+let five = document.querySelector("#five");
 
-let array = [twelve, one, two, pm];
+let array = [twelveam, oneam, twoam, nine, ten, eleven, twelve, one, two, three, four, five];
 console.log(array);
 
+// for (i = 0; i < array.length; i++) {
+//     if (a === "AM") {
+//         if (array[i].innerHTML.includes("AM")) {
+//             if (parseInt(array[i].innerHTML) % 12 > h % 12) {
+//                 array[i].parentNode.children[1].classList.add("future");
+//             } else if (parseInt(array[i].innerHTML) % 12 < h % 12) {
+//                 array[i].parentNode.children[1].classList.add("past");
+//             } else {
+//                 array[i].parentNode.children[1].classList.add("present");
+//             }
+//         } else {
+//             array[i].parentNode.children[1].classList.add("future");
+//         }
+//     } else {
+//         if (array[i].innerHTML.includes("PM")) {
+//             if (parseInt(array[i].innerHTML) % 12 > h % 12) {
+//                 array[i].parentNode.children[1].classList.add("future");
+//             } else if (parseInt(array[i].innerHTML) % 12 < h % 12) {
+//                 array[i].parentNode.children[1].classList.add("past");
+//             } else {
+//                 array[i].parentNode.children[1].classList.add("present");
+//             }
+//         } else {
+//             array[i].parentNode.children[1].classList.add("past");
+//         }
+//     }
+// }
+
 for (i = 0; i < array.length; i++) {
-    if (a === "AM") {
-        if (array[i].innerHTML.includes("AM")) {
-            console.log("It is AM");
-            console.log(parseInt(array[i].innerHTML));
-            if (parseInt(array[i].innerHTML) % 12 > h % 12) {
-                array[i].parentNode.children[1].classList.add("future");
-                console.log(array[i].innerHTML + ": future");
-            } else if (parseInt(array[i].innerHTML) % 12 < h % 12) {
-                array[i].parentNode.children[1].classList.add("past");
-                console.log(array[i].innerHTML + ": past");
-            } else {
-                array[i].parentNode.children[1].classList.add("present");
-                console.log(array[i].innerHTML + ": present");
-            }
-        } else {
+    if (a[0] === array[i].innerHTML[array[i].innerHTML.length - 2]) {
+        console.log("Its that time of day");
+        console.log(parseInt(array[i].innerHTML));
+        if (parseInt(array[i].innerHTML) % 12 > h % 12) {
             array[i].parentNode.children[1].classList.add("future");
             console.log(array[i].innerHTML + ": future");
-        }
-    } else {
-        if (array[i].innerHTML.includes("PM")) {
-            console.log("It is PM");
-            console.log(parseInt(array[i].innerHTML));
-            if (parseInt(array[i].innerHTML) % 12 > h % 12) {
-                array[i].parentNode.children[1].classList.add("future");
-                console.log(array[i].innerHTML + ": future");
-            } else if (parseInt(array[i].innerHTML) % 12 < h % 12) {
-                array[i].parentNode.children[1].classList.add("past");
-                console.log(array[i].innerHTML + ": past");
-            } else {
-                array[i].parentNode.children[1].classList.add("present");
-                console.log(array[i].innerHTML + ": present");
-            }
-        } else {
+        } else if (parseInt(array[i].innerHTML) % 12 < h % 12) {
             array[i].parentNode.children[1].classList.add("past");
             console.log(array[i].innerHTML + ": past");
+        } else {
+            array[i].parentNode.children[1].classList.add("present");
+            console.log(array[i].innerHTML + ": present");
         }
+    } else if (a === "AM") {
+        array[i].parentNode.children[1].classList.add("future");
+        console.log(array[i].innerHTML + ": future");
+    } else {
+        array[i].parentNode.children[1].classList.add("past");
+        console.log(array[i].innerHTML + ": past");
     }
 }
