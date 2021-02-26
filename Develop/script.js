a544c5733e2a37cc1100e531ecbb6122aaf6fcb3
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-02-26 14:48:44 -0500

    textarea values are now stored to local storage when the save buttons are clicked

--- Develop/script.js
+++ Develop/script.js
@@ -1,45 +1,44 @@
-let mom = moment().format("hA");
-console.log(mom);
-
+//Global variables defined
 let h = moment().format("h");
-console.log(h);
-
 let a = moment().format("A");
-console.log(a);
+let row = document.querySelectorAll(".row");
+let array = [];
+let save = document.querySelectorAll(".saveBtn");
 
-let nine = document.querySelector("#nine");
-let ten = document.querySelector("#ten");
-let eleven = document.querySelector("#eleven");
-let twelve = document.querySelector("#twelve");
-let one = document.querySelector("#one");
-let two = document.querySelector("#two");
-let three = document.querySelector("#three");
-let four = document.querySelector("#four");
-let five = document.querySelector("#five");
+//Creates an array of text elements that contains the times of each row
+for (i = 0; i < row.length; i++) {
+    let time = row[i].children[0];
+    array.push(time);
+}
 
-let array = [nine, ten, eleven, twelve, one, two, three, four, five];
-console.log(array);
-
+//Loops through the hours in the working day and color codes them based on whether they are in the past,
+//present or future
 for (i = 0; i < array.length; i++) {
     let text = array[i].innerHTML;
     if (a[0] === text[text.length - 2]) {
-        // console.log("Its that time of day");
-        // console.log(parseInt(text));
         if (parseInt(text) % 12 > h % 12) {
             array[i].parentNode.children[1].classList.add("future");
-            // console.log(text + ": future");
         } else if (parseInt(text) % 12 < h % 12) {
             array[i].parentNode.children[1].classList.add("past");
-            // console.log(text + ": past");
         } else {
             array[i].parentNode.children[1].classList.add("present");
-            // console.log(text + ": present");
         }
     } else if (a === "AM") {
         array[i].parentNode.children[1].classList.add("future");
-        // console.log(text + ": future");
     } else {
         array[i].parentNode.children[1].classList.add("past");
-        // console.log(text + ": past");
     }
+}
+
+//Creates an event listener for the save button of each hour and saves inputed text into local storage
+for (i = 0; i < save.length; i++) {
+    let time = save[i].parentNode.children[0].innerHTML;
+    let date = save[i].parentNode.children[1];
+    date.value = localStorage.getItem(time);
+
+    save[i].addEventListener("click", function() {    
+        date.value = this.parentNode.children[1].value;
+        localStorage.setItem(time, date.value);
+        console.log(time + ", " + date.value);
+    })
 }
